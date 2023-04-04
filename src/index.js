ce2fe5c96772c7c9cad644b4dbc672f3fa72e89d
Akshit Desai <codestromer@gmail.com>
2023-04-04 15:43:21 -0400

    added react page deployment info

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<HashRouter>
+    <Routes>
+        <Route path="/" element={<App/>} />
+    </Routes>
+</HashRouter>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
